a03fff9c9730d3823a8bedb1aea0da2d9b957212
Yassine Zaanouni <yassinezaanouni@gmail.com>
2024-12-18 18:15:07 +0100

    setup Nextjs Shadcn Tailwind boilerplate with strict eslint and prettier rules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    loader: "custom",
+    loaderFile: "./src/lib/imageLoader.ts",
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
 };
 
 export default nextConfig;
