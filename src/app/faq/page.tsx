b1a67adf5a7c2f05ff8f2d0dc3c4c8b585157dc1
Yassine Zaanouni <yassinezaanouni@gmail.com>
2025-01-04 18:40:35 +0100

    feat: add faq page

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { DashedLine } from '@/components/dashed-line';
+import { FAQ } from '@/components/sections/faq';
+import { Testimonials } from '@/components/sections/testimonials';
+
+const Page = () => {
+  return (
+    <div className="relative mx-2.5 mt-2.5 rounded-b-2xl rounded-t-[36px] bg-gradient-to-b from-[#F8F4E7] via-background to-background lg:mx-4">
+      <FAQ
+        className="py-28 text-center lg:pb-32 lg:pt-44"
+        className2="max-w-xl lg:grid-cols-1"
+      />
+      <DashedLine className="mx-auto max-w-xl" />
+      <Testimonials dashedLineClassName="hidden" className="!pb-0" />
+    </div>
+  );
+};
+
+export default Page;
--- src/components/sections/faq.tsx
+++ src/components/sections/faq.tsx
@@ -7,6 +7,8 @@ import {
   AccordionTrigger,
 } from '../ui/accordion';
 
+import { cn } from '@/lib/utils';
+
 const categories = [
   {
     title: 'Support',
@@ -60,43 +62,52 @@ const categories = [
   },
 ];
 
-export const FAQ = () => {
+export const FAQ = ({
+  className,
+  className2,
+}: {
+  className?: string;
+  className2?: string;
+}) => {
   return (
-    <section className="pb-28 lg:pb-32">
-      <div className="container max-w-5xl">
-        <div className="grid gap-16 lg:grid-cols-2">
-          <div className="space-y-4">
-            <h2 className="text-2xl font-semibold tracking-tight md:text-4xl lg:text-5xl">
-              Got Questions?
-            </h2>
-            <p className="max-w-md font-medium leading-snug text-muted-foreground">
-              If you can't find what you're looking for,{' '}
-              <Link href="/contact" className="underline underline-offset-4">
-                get in touch
-              </Link>
-              .
-            </p>
-          </div>
+    <section className={cn('pb-28 lg:pb-32', className)}>
+      <div
+        className={cn(
+          'container grid max-w-5xl gap-16 lg:grid-cols-2',
+          className2,
+        )}
+      >
+        <div className="space-y-4">
+          <h2 className="text-2xl font-semibold tracking-tight md:text-4xl lg:text-5xl">
+            Got Questions?
+          </h2>
+          <p className="max-w-md font-medium leading-snug text-muted-foreground lg:mx-auto">
+            If you can't find what you're looking for,{' '}
+            <Link href="/contact" className="underline underline-offset-4">
+              get in touch
+            </Link>
+            .
+          </p>
+        </div>
 
-          <div className="grid gap-6">
-            {categories.map((category, categoryIndex) => (
-              <div key={category.title} className="">
-                <h3 className="border-b py-4 font-medium text-muted-foreground">
-                  {category.title}
-                </h3>
-                <Accordion type="single" collapsible className="w-full">
-                  {category.questions.map((item, i) => (
-                    <AccordionItem key={i} value={`${categoryIndex}-${i}`}>
-                      <AccordionTrigger>{item.question}</AccordionTrigger>
-                      <AccordionContent className="text-muted-foreground">
-                        {item.answer}
-                      </AccordionContent>
-                    </AccordionItem>
-                  ))}
-                </Accordion>
-              </div>
-            ))}
-          </div>
+        <div className="grid gap-6 text-start">
+          {categories.map((category, categoryIndex) => (
+            <div key={category.title} className="">
+              <h3 className="border-b py-4 font-medium text-muted-foreground">
+                {category.title}
+              </h3>
+              <Accordion type="single" collapsible className="w-full">
+                {category.questions.map((item, i) => (
+                  <AccordionItem key={i} value={`${categoryIndex}-${i}`}>
+                    <AccordionTrigger>{item.question}</AccordionTrigger>
+                    <AccordionContent className="text-muted-foreground">
+                      {item.answer}
+                    </AccordionContent>
+                  </AccordionItem>
+                ))}
+              </Accordion>
+            </div>
+          ))}
         </div>
       </div>
     </section>
