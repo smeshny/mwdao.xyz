63c1d42dcc8ae10aaabacc92006e1f6bd77ee6b0
Yassine Zaanouni <yassinezaanouni@gmail.com>
2025-01-04 17:14:56 +0100

    feat: add Testimonials section with carousel functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@ import { Features } from '@/components/sections/features';
 import Hero from '@/components/sections/hero';
 import Logos from '@/components/sections/logos';
 import { ResourceAllocation } from '@/components/sections/resource-allocation';
+import { Testimonials } from '@/components/sections/testimonials';
 
 export default function Home() {
   return (
     <>
-      <div className="relative mx-2.5 mt-2.5 rounded-[36px] bg-gradient-to-b from-[#F8F4E7] via-muted to-muted/0 lg:mx-4">
+      <div className="relative mx-2.5 mt-2.5 rounded-b-2xl rounded-t-[36px] bg-gradient-to-b from-[#F8F4E7] via-muted to-muted/80 lg:mx-4">
         <Hero />
         <Logos />
         <Features />
         <ResourceAllocation />
       </div>
+      <Testimonials />
     </>
   );
 }
