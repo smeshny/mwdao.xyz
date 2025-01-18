4d51b839b591b4bd0ee6a607efd62335396f3128
Yassine Zaanouni <yassinezaanouni@gmail.com>
2025-01-18 13:43:46 +0100

    feat: add login and signup pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,82 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { FcGoogle } from 'react-icons/fc';
+
+import CreamContainer from '@/components/layout/cream-container';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Input } from '@/components/ui/input';
+
+const Login = () => {
+  return (
+    <CreamContainer>
+      <section className="py-28 lg:pb-32 lg:pt-44">
+        <div className="container">
+          <div className="flex flex-col gap-4">
+            <Card className="mx-auto w-full max-w-sm">
+              <CardHeader className="flex flex-col items-center space-y-0">
+                <Image
+                  src="/logo.svg"
+                  alt="logo"
+                  width={94}
+                  height={18}
+                  className="mb-7 dark:invert"
+                />
+                <p className="mb-2 text-2xl font-bold">Welcome back</p>
+                <p className="text-muted-foreground">
+                  Please enter your details.
+                </p>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-4">
+                  <Input type="email" placeholder="Enter your email" required />
+                  <div>
+                    <Input
+                      type="password"
+                      placeholder="Enter your password"
+                      required
+                    />
+                  </div>
+                  <div className="flex justify-between">
+                    <div className="flex items-center space-x-2">
+                      <Checkbox
+                        id="remember"
+                        className="border-muted-foreground"
+                      />
+                      <label
+                        htmlFor="remember"
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+                    <a href="#" className="text-sm font-medium text-primary">
+                      Forgot password
+                    </a>
+                  </div>
+                  <Button type="submit" className="mt-2 w-full">
+                    Create an account
+                  </Button>
+                  <Button variant="outline" className="w-full">
+                    <FcGoogle className="mr-2 size-5" />
+                    Sign up with Google
+                  </Button>
+                </div>
+                <div className="mx-auto mt-8 flex justify-center gap-1 text-sm text-muted-foreground">
+                  <p>Don&apos;t have an account?</p>
+                  <Link href="/signup" className="font-medium text-primary">
+                    Sign up
+                  </Link>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </section>
+    </CreamContainer>
+  );
+};
+
+export default Login;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,68 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { FcGoogle } from 'react-icons/fc';
+
+import CreamContainer from '@/components/layout/cream-container';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+
+const Signup = () => {
+  return (
+    <CreamContainer>
+      <section className="py-28 lg:pb-32 lg:pt-44">
+        <div className="container">
+          <div className="flex flex-col gap-4">
+            <Card className="mx-auto w-full max-w-sm">
+              <CardHeader className="flex flex-col items-center space-y-0">
+                <Image
+                  src="/logo.svg"
+                  alt="logo"
+                  width={94}
+                  height={18}
+                  className="mb-7 dark:invert"
+                />
+                <p className="mb-2 text-2xl font-bold">Start your free trial</p>
+                <p className="text-muted-foreground">
+                  Sign up in less than 2 minutes.
+                </p>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-4">
+                  <Input type="text" placeholder="Enter your name" required />
+                  <Input type="email" placeholder="Enter your email" required />
+                  <div>
+                    <Input
+                      type="password"
+                      placeholder="Enter your password"
+                      required
+                    />
+                    <p className="mt-1 text-sm text-muted-foreground">
+                      Must be at least 8 characters.
+                    </p>
+                  </div>
+                  <Button type="submit" className="mt-2 w-full">
+                    Create an account
+                  </Button>
+                  <Button variant="outline" className="w-full">
+                    <FcGoogle className="mr-2 size-5" />
+                    Sign up with Google
+                  </Button>
+                </div>
+                <div className="mx-auto mt-8 flex justify-center gap-1 text-sm text-muted-foreground">
+                  <p>Already have an account?</p>
+                  <Link href="/login" className="font-medium text-primary">
+                    Log in
+                  </Link>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </section>
+    </CreamContainer>
+  );
+};
+
+export default Signup;
