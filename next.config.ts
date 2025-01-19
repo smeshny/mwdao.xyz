0a4c2b6e1e6a94f43c3d66935da1fbfe060c0a52
Yassine Zaanouni <yassinezaanouni@gmail.com>
2025-01-19 10:12:35 +0100

    chore: remove customLoader

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-
   output: 'export',
   images: {
-    loader: 'custom',
-    loaderFile: './src/lib/imageLoader.ts',
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    unoptimized: true,
   },
 };
 const withMDX = createMDX({
