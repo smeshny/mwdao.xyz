4a010562c2490e715c5d6780697e0adccfbe722e
shanedownes <shanedownes01@gmail.com>
2025-05-15 20:50:45 +0100

    add preview origin to styleglide provider

--- src/components/styleglide-provider.tsx
+++ src/components/styleglide-provider.tsx
@@ -2,6 +2,9 @@
 
 import { KitViewProvider } from '@styleglide/kit-view-provider';
 
+const STYLEGLIDE_PREVIEW_ORIGIN =
+  'https://style-glide-monorepo-git-feature-ma-0f5cc9-shanedownes-projects.vercel.app';
+
 export function StyleGlideProvider() {
-  return <KitViewProvider />;
+  return <KitViewProvider origin={STYLEGLIDE_PREVIEW_ORIGIN} />;
 }
