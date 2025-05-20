48cd6c738c97919e50403602dff8591a82aeb52e
shanedownes <shanedownes01@gmail.com>
2025-05-21 00:49:36 +0100

    remove styleglide preview origin

--- src/components/styleglide-provider.tsx
+++ src/components/styleglide-provider.tsx
@@ -2,9 +2,6 @@
 
 import { KitViewProvider } from '@styleglide/kit-view-provider';
 
-const STYLEGLIDE_PREVIEW_ORIGIN =
-  'https://style-glide-monorepo-git-feature-ma-0f5cc9-shanedownes-projects.vercel.app';
-
 export function StyleGlideProvider() {
-  return <KitViewProvider origin={STYLEGLIDE_PREVIEW_ORIGIN} />;
+  return <KitViewProvider />;
 }
