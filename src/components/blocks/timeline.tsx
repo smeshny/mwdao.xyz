3f57b54dc05c6bbb1c6e43fd35a2c28a6e48bd65
SIARHEI PALISHCHUK <smeshny@gmail.com>
2025-10-31 13:40:41 +0100

    fix: increase bottom padding in timeline section for better spacing and layout

--- src/components/blocks/timeline.tsx
+++ src/components/blocks/timeline.tsx
@@ -124,7 +124,7 @@ export const Timeline = () => {
   });
 
   return (
-    <section id="timeline" className="py-0">
+    <section id="timeline" className="py-0 pb-16">
       <div className="container max-w-4xl">
         <h2 className="text-center text-3xl tracking-tight text-balance sm:text-4xl md:text-4xl lg:text-4xl">
           Tools development timeline
